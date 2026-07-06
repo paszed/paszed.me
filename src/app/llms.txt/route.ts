bd391ea1773cb1db3470d8e32a8b846ba94d8a8b
paszed <edvard.pasz@icloud.com>
2026-07-06 15:04:54 +0200

    feat: improve llms.txt for AI discoverability

--- src/app/llms.txt/route.ts
+++ src/app/llms.txt/route.ts
@@ -1,32 +1,78 @@
 import { site } from "@/config/site";
 
 export async function GET() {
+  const year = new Date().getFullYear();
+
   const body = `# ${site.name}
 
-Website: ${site.url}
-Owner: ${site.owner}
+The personal website of ${site.owner}.
 
-Description:
 ${site.description}
 
-Primary content:
-/about
-/projects
-/journal
-/uses
-/now
-/links
+## Primary Pages
 
-Sitemap:
-${site.url}/sitemap.xml
+- [Home](${site.url}/)
+- [About](${site.url}/about)
+- [Projects](${site.url}/projects)
+- [Journal](${site.url}/journal)
+- [Uses](${site.url}/uses)
+- [Now](${site.url}/now)
+- [Links](${site.url}/links)
+- [Search](${site.url}/search)
 
-Robots:
-${site.url}/robots.txt
+## Feeds
+
+- [RSS Feed](${site.url}/rss.xml)
+- [Sitemap](${site.url}/sitemap.xml)
+- [Robots](${site.url}/robots.txt)
+
+## Content
+
+This website contains:
+
+- Technical articles
+- Software engineering notes
+- Software architecture discussions
+- AI engineering experiments
+- Open-source projects
+- Developer tools
+- Personal software projects
+- Current work and learning updates
+
+## Topics
+
+- Software Engineering
+- Software Architecture
+- Artificial Intelligence
+- AI Engineering
+- TypeScript
+- Go
+- Next.js
+- React
+- Tailwind CSS
+- Developer Experience
+- Developer Tools
+- Linux
+
+## Author
+
+**${site.owner}**
+
+- Website: ${site.url}/
+- Email: <mailto:${site.email}>
+- GitHub: <https://github.com/paszed>
+- LinkedIn: <https://www.linkedin.com/in/edvardpasz/>
+
+## License
+
+Copyright © ${year} ${site.owner}. All rights reserved.
 `;
 
   return new Response(body, {
     headers: {
-      "Content-Type": "text/plain; charset=utf-8",
+      "Content-Type": "text/markdown; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+      "X-Robots-Tag": "noindex",
     },
   });
 }
