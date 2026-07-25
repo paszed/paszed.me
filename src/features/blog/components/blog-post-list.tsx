20bd40db667daba699401058f11929d242e1e32d
Edvard Pasz <edvard@paszed.me>
2026-07-25 16:41:07 +0200

    Remove obsolete site routes

--- src/features/blog/components/blog-post-list.tsx
+++
@@ -1,9 +0,0 @@
-import { EmptyState } from "@/design-system";
-
-export function BlogPostList() {
-  return (
-    <EmptyState
-      title="The first article is coming soon."
-    />
-  );
-}
--- src/features/blog/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { BlogHero } from "./blog-hero";
-export { BlogPostList } from "./blog-post-list";
-
-export type * from "./blog-hero";
-export type * from "./blog-post-list";
